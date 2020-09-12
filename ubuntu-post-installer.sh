d039ec081b7585959da80cb0284e9044cdcaac55
Joshua <joshuagalinato@gmail.com>
2020-09-12 16:51:19 +0200

    add post installer

---
+++ ubuntu-post-installer.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#
+# Make sure to update permissions with:
+# sudo chmod +x ~/ubuntu-post-installer.sh
+#
+# Run the script with:
+# sudo bash ubuntu-post-installer.sh   
+
+# apt
+sudo apt install firefox git jpegoptim ripgrep thunderbird tmux tree vlc gimp inkscape bleachbit -y 
+
+# snaps
+sudo snap install atom --classic
+sudo snap install obs-studio
+sudo snap install translatium
+sudo snap install node --classic
+sudo snap install code --classic
+sudo snap install signal-desktop
+sudo snap install discord
+sudo snap install spotify
+sudo snap install slack --classic
+sudo snap install skype --classic
+sudo snap install chromium
+sudo snap install handbrake-jz
+sudo snap install tor
+sudo snap install bitwarden
+sudo snap install audacity
